d83f17276ca30b267c94b654d860d44766c99d82
yanyanshidalianmao <yanyandeyx123@sina.com>
2016-12-20 19:23:57 +0800

    123

---
+++ 大数医达/js/demo.js
@@ -0,0 +1,8 @@
+$(function(){
+	window.onResize=function(){
+        document.documentElement.style.fontSize=document.documentElement.clientWidth/3.75+'px';
+    }
+    document.documentElement.style.fontSize=document.documentElement.clientWidth/3.75+'px';
+});
+
+
